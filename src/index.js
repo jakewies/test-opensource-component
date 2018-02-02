10d2935ea831bfdf17ee4691d431674de5f24164
Jake Wiesler <jakewiesler@gmail.com>
2018-02-02 07:35:52 -0500

    initial commit.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const TestComponent = styled.h1`
+  font-size: 5rem;
+  font-weight: bold;
+  color: palevioletred;
+  background: papayawhip;
+`
+
+export default TestComponent
